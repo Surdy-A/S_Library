81dba723c2a1c8fa6c25235c41171b211faa8e73
Surdyhey <sodiq.ajayi@yahoo.com>
2020-08-08 18:40:53 +0100

    Add a Search Page

--- books/views.py
+++ books/views.py
@@ -113,8 +113,7 @@ class SearchResultsListView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         return Book.objects.filter(
-            Q(title__icontains=query) | Q(author__icontains=query)
-        )
+            Q(title__icontains=query) | Q(description__icontains=query))
 
 
 class ReviewViewForm(FormView):
